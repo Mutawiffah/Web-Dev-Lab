73abdc26d68b1deddf890ccd5626a719a4178d02
Mutawiffah <mutawiffahmudassarkhan@gmail.com>
2025-06-17 02:35:14 +0500

    changes

--- server.js
+++ server.js
@@ -1,45 +1,77 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const expressLayouts = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const authRoutes = require("./routes/authRoutes");
 
 const server = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/web-auth", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+server.use(express.static(path.join(__dirname, "public")));
 
 server.use(express.urlencoded({ extended: true }));
-server.use(express.static("public"));
-server.use(expressLayouts);
-
-server.set("view engine", "ejs");
-server.set("layout", "layouts/main");
+server.use(cookieParser());
 
 server.use(
   session({
     secret: "mySecret",
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      maxAge: 60000 * 60 * 24,
+      httpOnly: true,
+      secure: false,
+    },
   })
 );
+
+server.set("view engine", "ejs");
+server.use(expressLayouts);
+server.set("layout", "layout"); 
+
+server.use((req, res, next) => {
+  res.locals.user = req.session.userId ? true : null;
+  next();
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/webproducts")
+  .then(() => console.log("✅ MongoDB connected successfully"));
 
 server.use("/", authRoutes);
 
 server.get("/cv", (req, res) => {
-  res.render("cv", { layout: false });
+  res.render("cv", { title: "CV" });
 });
 
 server.get("/landingpage", (req, res) => {
   if (!req.session.userId) return res.redirect("/login");
-  res.render("landingpage");
+  res.render("landingpage", { title: "Home", pageStyle: "style" });
+});
+
+server.get("/", (req, res) => {
+  if (!req.session.userId) return res.redirect("/login");
+  res.redirect("/landingpage");
+});
+
+server.get("/login", (req, res) => {
+  res.render("login", { title: "Login", pageStyle: "login" });
+});
+
+server.get("/signup", (req, res) => {
+  res.render("register", { title: "Register", pageStyle: "register" });
 });
 
 server.get("/", (req, res) => {
   res.redirect("/landingpage");
+});
+
+server.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/login");
+  });
 });
 
 server.listen(4000, () => {
